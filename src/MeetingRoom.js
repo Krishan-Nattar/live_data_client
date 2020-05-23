b76ca36ce8b93c39a526bd1c4c535152af7af1e6
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-22 20:40:17 -0700

    adding cat movements

--- src/MeetingRoom.js
+++ src/MeetingRoom.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
+// import { useEffect } from 'react';
 
 const MeetingRoom = (props) => {
+	const [xAxis, setXaxis] = useState(25);
+    const [yAxis, setYaxis] = useState(25);
+    const [catMessage, setCatMessage] = useState(false)
+
 	let io = socketIOClient('https://web-rtc-gameserver.herokuapp.com/');
+	useEffect(() => {
+		io.emit('ready', {
+			chat_room: ROOM,
+			signaling_room: SIGNALING_ROOM,
+			my_name: props.name,
+		});
+
+		io.on('announce', (data) => {
+			processData(data.message);
+		});
+
+		io.on('message', (data) => {
+			// processData(data.message);
+			processData(data.author + ': ' + data.message);
+        });
+        
+        io.on('move', (data) => {
+            moveCharacter(data.direction);
+		});
+	}, []);
+
 	let ROOM = props.channel;
 	let SIGNALING_ROOM = `${props.channel}z`;
 
@@ -12,13 +38,54 @@ const MeetingRoom = (props) => {
 		newMessage.textContent = message;
 		if (outsideSource) {
 			newMessage.style.color = 'blue';
+			// console.log(message)
+			// console.log(parseInt(message));
+            // let myNumber = message.split(' ')
+            // meow();
+			// setXaxis((prev) => {
+			// 	return prev + parseInt(message);
+			// });
+			// setYaxis((prev) => {
+			// 	return prev + 10;
+			// });
 		} else {
 			newMessage.style.color = 'green';
 		}
 		messageContainer.appendChild(newMessage);
-	}
+    }
 
-	const sendData = (e) => {
+    function moveCharacter(direction){
+        if(direction==="up"){
+
+        } else if(direction==="down"){
+
+        } else if(direction==="left"){
+
+        } else if(direction==="right"){
+
+        } 
+        console.log(direction);
+    }
+    function meowOff() {
+        setCatMessage(false);
+    }
+    function meow(){
+        setCatMessage(true);
+        setTimeout(meowOff, 3000);
+    }
+
+    function sendMove(direction) {
+        
+        if(direction === "up" || direction === "down" || direction === "left" || direction === "right"){
+
+        }
+        
+        io.emit('move', {
+			direction
+		});
+    }
+
+	function sendData(e) {
 		e.preventDefault();
 		let input = document.getElementById('inputMessage').value;
 		let myName = props.name;
@@ -30,29 +97,29 @@ const MeetingRoom = (props) => {
 		});
 
 		processData(myName + ': ' + input, false);
+	}
+
+	const style = {
+		transform: `translate(${xAxis}px, ${yAxis}px)`,
 	};
-
-	io.emit('ready', {
-		chat_room: ROOM,
-		signaling_room: SIGNALING_ROOM,
-		my_name: props.name,
-	});
-
-	io.on('announce', (data) => {
-		processData(data.message);
-	});
-
-	io.on('message', (data) => {
-		processData(data.author + ': ' + data.message);
-	});
 
 	return (
 		<div>
+			<div style={style}>
+				<img src="./cat.gif" />
+                {catMessage && <p>"MEOW"</p>}
+			</div>
 			<p>Hello {props.name}!</p>
 			<form onSubmit={sendData}>
 				<input type="text" placeholder="message" id="inputMessage" />
 				<button type="submit">SEND MESSAGE?</button>
 			</form>
+
+			<button onClick={sendMove.bind(this, "up")}>UP</button>
+            <button onClick={sendMove.bind(this, "down")}>DOWN</button>
+            <button onClick={sendMove.bind(this, "left")}>LEFT</button>
+            <button onClick={sendMove.bind(this, "right")}>RIGHT</button>
+
 			<div className="messageContainer"></div>
 		</div>
 	);
