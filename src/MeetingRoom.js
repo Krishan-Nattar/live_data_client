62fe6b50cf273055e27f1b885832e7b3fa14bee1
Krishan Nattar <krishan.nattar@gmail.com>
2020-05-22 17:24:52 -0700

    initial meeting room component created.

---
+++ src/MeetingRoom.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+
+
+const MeetingRoom = (props) => {
+    return ( 
+        <div>
+            <p>Meeting ROom!</p>
+        </div>
+     );
+}
+ 
+export default MeetingRoom;
